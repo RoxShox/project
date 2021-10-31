5d7b67dfd048cacb0c2d386f59aa1990a3a457c4
Vladimir <example@gmail.com>
2021-10-31 18:44:41 +0300

    first coomit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// "use strict";
+
+// let str = 5;
+// const leftBorderWidth = 1;
+
+// str = 10;
+// console.log(str);
+
+// const obj ={
+//     a: 50
+// }
+
+// obj.a = 10;
+// console.log(obj);
+
+// {
+//     let result = 50;
+    
+// }
+// console.log(result);
+
+
+// alert(5);
+// [].push('a');
+
+const answers = [];
+
+answers[0] = prompt ('Как ваше имя?', '');
+answers[1] = prompt ('Как ваше фамилия?', '');
+answers[2] = prompt ('Сколько вам лет?', '');
+
