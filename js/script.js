0c4c8d5618fb7abb713b6581de2b22179ad9ee45
Vladimir <example@gmail.com>
2021-11-08 17:14:27 +0300

    JsLesson

--- js/script.js
+++ js/script.js
@@ -1,20 +1,86 @@
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', " " );
+let numberOfFilms; 
+
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', " " );
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', " " );
+
+    }
+}
+
+start();
 
 let obj = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt('На сколько оцените его?', ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt('На сколько оцените его?', "");
 
 
-    obj.movies[a] = b;
-    obj.movies[c] = d;
+ 
 
-    console.log(obj);
+    function rememberMyFilms() {
+        for( let i = 0; i < 2; i++) {
+            let a = prompt("Один из последних просмотренных фильмов?", ""),
+            b = prompt('На сколько оцените его?', "");
+    
+            if(a != null && b != null && a!= '' && b != "" && a.length < 50) {
+                obj.movies[a] = b;
+                console.log('done');
+    
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    }
+
+    // rememberMyFilms();
+
+
+    function detectPersonalLevel() {
+        if( obj.count < 10) {
+            console.log('Просмотрено довольно мало фильмов');
+        } else if (obj.count >= 10 && obj.count <= 30) {
+            console.log('Вы классический зритель');
+        } else if (obj.count > 30) {
+            console.log('Киноман');
+        }
+         else{
+             console.log('Ошибка');
+         }
+            console.log(obj); 
+    }
+
+    // detectPersonalLevel();
+
+    function showMyDB (hidden) {
+        if (!hidden) {
+            console.log(obj);
+        }
+    }
+
+    showMyDB(obj.privat);
+
+    function writeYourgeners () {
+        for ( let i = 1; i < 4; i++ ) {
+            const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+            obj.genres[i - 1] = genre;
+        }
+    }
+
+    writeYourgeners();
+
+    
+
+    
+
+
+
+
+
